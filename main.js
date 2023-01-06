5379835684f7713cadbef1b0568de8a636f72c04
unknown <f.ugolini@grupocermob.com.br>
2023-01-06 14:01:14 -0300

    colocando length no contador

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 //enquanto
-while (contador < 9) {
+while (contador < listaDeTeclas.length) {
     listaDeTeclas[contador].onclick = tocaSomPom;
     contador ++;
     console.log(contador);
